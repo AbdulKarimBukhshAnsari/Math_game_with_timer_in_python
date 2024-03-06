eda973a19272b9bb14709ca8267c89b262232629
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-03-05 18:33:40 -0800

    complete the code except timer

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import random as rd
+import math
 
-
-def problems(start,end,problems):
-    right_operand = rd.randint(start,end)
-    left_operand = rd.randint(start,end)
+def problems(a,b):
+    
+    operators = ["+","-","*","/","%"]
+    right_operand = str(rd.randint(a,b))
+    left_operand = str(rd.randint(a,b))
+    operators_choice = rd.choice(operators)
+    quesion = left_operand +" "+operators_choice+" "+right_operand
+    answer = eval(quesion)
+    
+    return quesion,answer
     
 
 intro = "This game is design to check your maths ability."
@@ -11,8 +18,21 @@ starting ="Hello,The greater would be the number the greater would be the diffic
 print(intro.center(60,"*"))
 print(starting)
 
+
 start = int(input("Enter the start of the number: "))
 end = int(input("Enter the end of the number: "))
-problems = int(input("Enter nymber of problems: "))
+problem = int(input("number of problems: "))
+checker = 0
 
-problems(start,end,problems)
+for i in range(problem):
+    question , answer =problems(start,end)
+    while True:
+        sol = float(input(f"Question #{i+1} : {question} = "))
+        if sol == answer:
+            print(f"You guessed the correct answer after {checker+1} turn! ")
+            break
+        else:
+            print("You guessed wrong Try Again!!")
+            checker+=1
+            
+        
